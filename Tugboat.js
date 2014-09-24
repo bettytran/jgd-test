ac41f48e87844f527c673f904c68eeb885227e27
blakehall <blake@lullabot.com>
2014-09-24 13:13:55 -0500

    changing formatting for a local Tugboat.js file.

--- Tugboat.js
+++ Tugboat.js
@@ -1,3 +1,13 @@
-var config = {};
-config.testingVariableLoading = 'you did it!';
+#!/usr/bin/env node
+
+var config = {
+  project: {},
+  environment: {}
+};
+
+config.project.hardlinks = true;
+config.project.mergeBranch = 'master';
+
+config.environment.githubRepo = 'BROKEN';
+
 module.exports = config;
